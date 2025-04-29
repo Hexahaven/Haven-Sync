bb614f7de7bd9cac6331bb3bc3cf9d8951783930
GaneshPrasad14 <128919436+GaneshPrasad14@users.noreply.github.com>
2025-04-29 12:27:06 +0530

    Update imports.js

--- src/imports/imports.js
+++ src/imports/imports.js
@@ -1,6 +1,8 @@
-export {default as HexaDashboard} from '../features/HexaDashboard';
-export {default as HexaDevices} from '../features/HexaDevices';
-export {default as HexaEditProfile} from '../features/HexaEditProfile';
-export {default as HexaWelcomeScreen} from '../features/HexaWelcomeScreen';
-export {default as HexaLoginScreen} from '../features/HexaLoginScreen';
-export {default as HexaSignUpScreen} from '../features/HexaSignUpScreen';
+export { default as HexaDashboard } from '../features/HexaDashboard';
+export { default as HexaDevices } from '../features/HexaDevices';
+export { default as HexaEditProfile } from '../features/HexaEditProfile';
+export { default as HexaWelcomeScreen } from '../features/HexaWelcomeScreen';
+export { default as HexaLoginScreen } from '../features/HexaLoginScreen';
+export { default as HexaSignUpScreen } from '../features/HexaSignUpScreen';
+export { default as HexaSettings } from '../features/HexaSettings';
+export { default as HexaSensorAutomation } from '../features/HexaSensorAutomation';
